88f5ff84f7cf20b8ca4f7e98ae6963c4dfaac873
Mikanixonable <47976119+Mikanixonable@users.noreply.github.com>
2023-12-21 23:46:37 +0900

    4版

--- csv2md.py
+++ csv2md.py
@@ -49,7 +49,9 @@ def dic2md(dic):
             for i in range(len(lang['言語名']) - 1):
                 name += ', '+lang['言語名'][i+1]
 
+     
         year = lang['年代'][0]
+ 
 
 
         artist = ''
@@ -92,21 +94,41 @@ def dic2md(dic):
 
 md = """
 
-# 日本語圏人工言語リスト(2023年12月)
+# 日本語圏人工言語リスト4版(2023年12月)
 　日本語圏の人工言語のリストが最近作られていないと思ったので、底となるリストを参考に、新しい言語も合わせてつくったものです。リストのすべての言語の情報は私がアクセスして確認しなおしています。
 　このリストは後で作るリストのための下準備という位置づけで、人工言語として言及されているものは見つけ次第すべて入れています。そのため、創作世界の設定に名前だけ登場するような言語や、ツイッター上で思い付きで作られた言語も含んでいます。
 　言語の説明やCLAコードの入った生データは以下のリンクからアクセスできます。tsv(タブ区切り値)形式であり、Excelから開けます。私は著作権を放棄するので、利用したい方は使ってください。
-https://mikanixonable.github.io/data/conlang.tsv
 
-追記 utf-8でcsv形式に書き直しました
-https://mikanixonable.github.io/data/conlang.csv
+参照用url: https://mikanixonable.github.io/conlangList/conlang.csv
+GitHub:  https://github.com/Mikanixonable/conlangList/blob/main/conlang.csv
+更新履歴(GitHub): https://github.com/Mikanixonable/conlangList/commits/main/conlang.csv
+
+1版 2023-12-17
+2版 2023-12-18
+3版 2023-12-19
+4版 2023-12-21
 
 ## 底としたリスト
 - [人工言語リスト 日本人による人工言語（アイウエオ順）](http://dos.chottu.net/conlang_link.html?l=index) - 2nd LVG IMG.The Second Living Image.
 2004年
 
+- [人工言語憩いの場アーカイブ](https://w.atwiki.jp/kakis/pages/5471.html#id_1b7f8ccd) - atwiki（アットウィキ） 
+2007年
+
 - [人工言語リンク集](https://conlinguistics.org/link.html) - 人工言語学
 2012年
+
+- [架空の言語一覧](https://japan.fandom.com/wiki/%E6%9E%B6%E7%A9%BA%E3%81%AE%E8%A8%80%E8%AA%9E%E4%B8%80%E8%A6%A7) - 架空の言語一覧 | Japan | Fandom
+2014年
+
+- [アルカ-リンク](https://w.atwiki.jp/kursodeesperanto/pages/36.html) - はじめてのエスペラント - atwiki（アットウィキ）  
+2014年
+
+- [世界模擬実験塔設定集](https://w.atwiki.jp/koreori/) - atwiki（アットウィキ） 
+2015年
+
+- [Faras' Room](https://sites.google.com/site/faraspalt/links?authuser=0 ) - Links 
+2015年ごろ?
 
 - [言語記事一覧](https://conlinguistics.fandom.com/ja/wiki/%E8%A8%80%E8%AA%9E%E8%A8%98%E4%BA%8B%E4%B8%80%E8%A6%A7) - 人工言語wiki
 2016年ごろと思われる
@@ -123,6 +145,9 @@ https://mikanixonable.github.io/data/conlang.csv
 - [辞書リスト](http://twoc.ever.jp/twoc/conlang.cgi?mode=list) - The world of conlangs
  2020年ごろ
 
+- [人工言語 柳霞](https://sites.google.com/view/ryuuka/k-ren-gong-yan-yurinku-ji?authuser=0) - J．人工言語リンク集
+2020年ごろ?
+
 - [人工言語リスト](https://sites.google.com/site/moyacilang/conlanglist) - slaimsan
 たぶんもっとも網羅的
 2020年
@@ -138,25 +163,13 @@ https://mikanixonable.github.io/data/conlang.csv
 - [架空言語](https://tanukipedia.miraheze.org/wiki/%E6%9E%B6%E7%A9%BA%E8%A8%80%E8%AA%9E) - Tanukipedia (タヌキペディア) 
 2023年
 
-- [人工言語 柳霞](https://sites.google.com/view/ryuuka/k-ren-gong-yan-yurinku-ji?authuser=0) - J．人工言語リンク集
-2020年ごろ?
-
-- [架空の言語一覧](https://japan.fandom.com/wiki/%E6%9E%B6%E7%A9%BA%E3%81%AE%E8%A8%80%E8%AA%9E%E4%B8%80%E8%A6%A7) - 架空の言語一覧 | Japan | Fandom
-
-- [アーカイブ](https://w.atwiki.jp/kakis/pages/5471.html#id_1b7f8ccd) - atwiki（アットウィキ） 
-
-- [Faras' Room](https://sites.google.com/site/faraspalt/links?authuser=0 ) - Links 
-
-- [アルカ-リンク](https://w.atwiki.jp/kursodeesperanto/pages/36.html) - はじめてのエスペラント - atwiki（アットウィキ）  
-
-- [世界模擬実験塔設定集](https://w.atwiki.jp/koreori/) - atwiki（アットウィキ） 
 """
 
 dic = csv2dic('conlang.csv')
 for lang in dic:
     if '年代' not in lang:
         print(lang['言語名'])
-pprint(dic)
+# pprint(dic)
 dic = sorted(dic, key=lambda x: int(x['年代'][0][0:4]))
 
 
